38851967d71786792df2deedc76139b0993cb7e6
lmboldrin <lucas.m.boldrin@gmail.com>
2025-02-24 00:15:31 -0300

    Adicionando o navbar

--- next-frontend/src/app/layout.tsx
+++ next-frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`{h-screen flex flex-col}`}>
+      <body className={`h-screen flex flex-col`}>
+      <Navbar />
         <div className="container mx-auto px-4 flex flex-grow">{children}</div>
       </body>
     </html>
--- next-frontend/src/components/Navbar.tsx
+++ next-frontend/src/components/Navbar.tsx
@@ -13,7 +13,7 @@ import { useSearchParams } from "next/navigation";
 
 export default function Navbar() {
   const searchParams = useSearchParams();
-  const wallet_id = searchParams.get("wallet_id");
+  const walletId = searchParams.get("walletId");
   return (
     <FlowbiteNavbar fluid rounded>
       <NavbarBrand href="https://flowbite-react.com">
@@ -28,18 +28,18 @@ export default function Navbar() {
       </NavbarBrand>
       <div className="flex md:order-2">
         <div className="content-center">
-          Olá {wallet_id?.substring(0, 5)}...
+          Olá {walletId?.substring(0, 5)}...
         </div>
         <NavbarToggle />
       </div>
       <NavbarCollapse>
-        <Link href={`/?wallet_id=${wallet_id}`} passHref legacyBehavior>
+        <Link href={`/?walletId=${walletId}`} passHref legacyBehavior>
           <NavbarLink className="text-xl">Carteira</NavbarLink>
         </Link>
-        <Link href={`/assets/?wallet_id=${wallet_id}`} passHref legacyBehavior>
+        <Link href={`/assets/?walletId=${walletId}`} passHref legacyBehavior>
           <NavbarLink className="text-xl">Ativos</NavbarLink>
         </Link>
-        <Link href={`/orders?wallet_id=${wallet_id}`} passHref legacyBehavior>
+        <Link href={`/orders?walletId=${walletId}`} passHref legacyBehavior>
           <NavbarLink href="#" className="text-xl">
             Ordens
           </NavbarLink>
